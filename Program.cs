e1ab89aa172119db30882cca8727bf698c4ccc1a
Artemii Ryabov <a89056602510@bk.ru>
2020-04-30 12:23:00 +0300

    Feature 2

--- Program.cs
+++ Program.cs
@@ -11,9 +11,13 @@ namespace GitTest
         public static int sum(int a, int b) {
             return a + b;
         }
+        public static int div(int a, int b) {
+            return a - b;
+        }
         public static void Main() {
             Console.WriteLine("Hello world");
             Console.WriteLine(sum(3, 5));
+            Console.WriteLine(div(3, 5));
             Console.ReadLine();
         }
     }
